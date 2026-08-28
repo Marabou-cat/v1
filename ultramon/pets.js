75bab6d5743830284b0eab0fe6061667fdd0e95b
Marabou-cat <kaizhewei2020@gmail.com>
2026-08-28 18:18:53 +0200

    Update pets.js

--- ultramon/pets.js
+++ ultramon/pets.js
@@ -1,23 +1,23 @@
 // Database of Pet Species with Base Stats (Lv. 1) and Max Stats (Lv. 100)
 const PETS = {
-    emberkin: {
-        id: "emberkin",
-        name: "Emberkin",
+    flaragon: {
+        id: "flaragon",
+        name: "Flaragon",
         type: "Fire",
-        baseStats: { hp: 20, attack: 14, defense: 10, spAttack: 16, spDefense: 12, speed: 15 },
-        maxStats:  { hp: 210, attack: 160, defense: 120, spAttack: 190, spDefense: 140, speed: 175 }
+        baseStats: { hp: 20, attack: 10, defense: 10, spAttack: 16, spDefense: 12, speed: 15 },
+        maxStats:  { hp: 210, attack: 110, defense: 120, spAttack: 190, spDefense: 140, speed: 175 }
     },
-    aqualeaf: {
-        id: "aqualeaf",
-        name: "Aqualeaf",
+    bubbitty: {
+        id: "bubbity",
+        name: "Bubbitty",
         type: "Water",
         baseStats: { hp: 22, attack: 10, defense: 14, spAttack: 12, spDefense: 16, speed: 10 },
         maxStats:  { hp: 230, attack: 130, defense: 170, spAttack: 150, spDefense: 195, speed: 120 }
     },
-    terrawulf: {
-        id: "terrawulf",
-        name: "Terrawulf",
-        type: "Earth",
+    sproupup: {
+        id: "sproupup",
+        name: "Sproupup",
+        type: "Grass",
         baseStats: { hp: 25, attack: 16, defense: 13, spAttack: 8, spDefense: 10, speed: 12 },
         maxStats:  { hp: 260, attack: 185, defense: 155, spAttack: 100, spDefense: 130, speed: 145 }
     },
