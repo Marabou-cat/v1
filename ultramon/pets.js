856f8a775cc2ee838f44e3ecfae63f3bc2a057e9
Marabou-cat <kaizhewei2020@gmail.com>
2026-08-29 16:21:31 +0200

    Update pets.js

--- ultramon/pets.js
+++ ultramon/pets.js
@@ -30,10 +30,10 @@ const PETS = {
         type: ["fire"],
         spawn_routes: [],
         img: "petpng/flaragon.png",
-        baseStats: { hp: 50, attack: 10, defense: 10, spAttack: 16, spDefense: 12, speed: 15 },
-        maxStats:  { hp: 250, attack: 110, defense: 120, spAttack: 190, spDefense: 140, speed: 175 },
+        baseStats: { hp: 100, attack: 10, defense: 10, spAttack: 16, spDefense: 12, speed: 15 },
+        maxStats:  { hp: 500, attack: 110, defense: 120, spAttack: 190, spDefense: 140, speed: 175 },
         moves: [
-            { name: "Scratch", type: "basic", damage: 15, powerCost: 0, damageType: "physical", levelToLearn: 1 },
+            { name: "Scratch", type: "basic", damage: 10, powerCost: 0, damageType: "physical", levelToLearn: 1 },
             { name: "Ember", type: "fire", damage: 25, powerCost: 5, damageType: "special", levelToLearn: 1 },
             { name: "Flame Dash", type: "fire", damage: 45, powerCost: 10, damageType: "physical", levelToLearn: 5 },
             { name: "Inferno Blast", type: "fire", damage: 70, powerCost: 20, damageType: "special", levelToLearn: 10 }
@@ -45,8 +45,8 @@ const PETS = {
         type: ["water"],
         spawn_routes: [],
         img: "petpng/bubbitty.png",
-        baseStats: { hp: 55, attack: 10, defense: 14, spAttack: 12, spDefense: 16, speed: 10 },
-        maxStats:  { hp: 270, attack: 130, defense: 170, spAttack: 150, spDefense: 195, speed: 240 },
+        baseStats: { hp: 110, attack: 10, defense: 14, spAttack: 12, spDefense: 16, speed: 10 },
+        maxStats:  { hp: 540, attack: 130, defense: 170, spAttack: 150, spDefense: 195, speed: 240 },
         moves: [
             { name: "Tackle", type: "basic", damage: 15, powerCost: 0, damageType: "physical", levelToLearn: 1 },
             { name: "Water Gun", type: "water", damage: 25, powerCost: 5, damageType: "special", levelToLearn: 1 },
@@ -60,8 +60,8 @@ const PETS = {
         type: ["grass"],
         spawn_routes: [],
         img: "petpng/sproupup.png",
-        baseStats: { hp: 60, attack: 16, defense: 13, spAttack: 8, spDefense: 10, speed: 12 },
-        maxStats:  { hp: 300, attack: 185, defense: 155, spAttack: 100, spDefense: 130, speed: 145 },
+        baseStats: { hp: 120, attack: 16, defense: 13, spAttack: 8, spDefense: 10, speed: 12 },
+        maxStats:  { hp: 600, attack: 185, defense: 155, spAttack: 100, spDefense: 130, speed: 145 },
         moves: [
             { name: "Tackle", type: "basic", damage: 15, powerCost: 0, damageType: "physical", levelToLearn: 1 },
             { name: "Vine Whip", type: "grass", damage: 25, powerCost: 5, damageType: "physical", levelToLearn: 1 },
@@ -75,8 +75,8 @@ const PETS = {
         type: ["electric"],
         spawn_routes: [1, 2],
         img: "petpng/sparkwing.png",
-        baseStats: { hp: 45, attack: 13, defense: 8, spAttack: 17, spDefense: 9, speed: 18 },
-        maxStats:  { hp: 230, attack: 150, defense: 105, spAttack: 205, spDefense: 115, speed: 210 },
+        baseStats: { hp: 95, attack: 13, defense: 8, spAttack: 17, spDefense: 9, speed: 18 },
+        maxStats:  { hp: 450, attack: 150, defense: 105, spAttack: 205, spDefense: 115, speed: 210 },
         moves: [
             { name: "Quick Peck", type: "basic", damage: 15, powerCost: 0, damageType: "physical", levelToLearn: 1 },
             { name: "Thundershock", type: "electric", damage: 25, powerCost: 6, damageType: "special", levelToLearn: 1 },
@@ -90,8 +90,8 @@ const PETS = {
         type: ["grass", "fire"],
         spawn_routes: [1, 2],
         img: "petpng/coalapling.png",
-        baseStats: { hp: 60, attack: 17, defense: 25, spAttack: 17, spDefense: 25, speed: 10 },
-        maxStats:  { hp: 290, attack: 150, defense: 180, spAttack: 150, spDefense: 180, speed: 120 },
+        baseStats: { hp: 120, attack: 17, defense: 25, spAttack: 17, spDefense: 25, speed: 10 },
+        maxStats:  { hp: 650, attack: 150, defense: 180, spAttack: 150, spDefense: 180, speed: 120 },
         moves: [
             { name: "Stick Impact", type: "basic", damage: 12, powerCost: 0, damageType: "physical", levelToLearn: 1 },
             { name: "Burn Out", type: "fire", damage: 50, powerCost: 40, damageType: "special", levelToLearn: 1 },
@@ -105,8 +105,8 @@ const PETS = {
         type: ["combat","bug"],
         spawn_routes: [1, 2],
         img: "petpng/samupillar.png",
-        baseStats: { hp: 45, attack: 15, defense: 12, spAttack: 12, spDefense: 5, speed: 5 },
-        maxStats:  { hp: 220, attack: 125, defense: 120, spAttack: 95, spDefense: 60, speed: 100 },
+        baseStats: { hp: 80, attack: 15, defense: 12, spAttack: 12, spDefense: 5, speed: 5 },
+        maxStats:  { hp: 350, attack: 125, defense: 120, spAttack: 95, spDefense: 60, speed: 100 },
         moves: [
             { name: "Sticky Webs", type: "bug", damage: 12, powerCost: 0, damageType: "physical", levelToLearn: 1 },
             { name: "Power Punch", type: "combat", damage: 50, powerCost: 40, damageType: "physical", levelToLearn: 1 },
