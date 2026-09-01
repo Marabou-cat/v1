3eb3d17c59af769c6937f203c87feef74c61aa82
Marabou-cat <kaizhewei2020@gmail.com>
2026-09-01 20:55:52 +0200

    Update pets.js

--- ultramon/pets.js
+++ ultramon/pets.js
@@ -255,7 +255,7 @@ const PETS = {
         name: "Shocket",
         type: ["water"],
         spawn_routes: [3],
-        img: "petpng/shocket.png",
+        img: "petpng/pyrodon.png",
         catchRate: 45,
         evolvingLevel: 20,
         evolutionId: "trishock",
