81316883ac7c309d78641311f527ebe994cf0338
Marabou-cat <kaizhewei2020@gmail.com>
2026-09-02 17:27:21 +0200

    Update pets.js

--- ultramon/pets.js
+++ ultramon/pets.js
@@ -1,15 +1,17 @@
 const TYPE_CHART = {
-    fire:     { fire: 0.67, water: 0.67, grass: 1.5,  electric: 1.0, combat: 1.0, basic: 1.0, bug: 1.5, dragon: 0.67, air: 1.0, metal: 1.5 },
-    water:    { fire: 1.5,  water: 0.67, grass: 0.67, electric: 1.0, combat: 1.0, basic: 1.0, bug: 1.0, dragon: 0.67, air: 1.0, metal: 1.0 },
-    grass:    { fire: 0.67, water: 1.5,  grass: 0.67, electric: 1.0, combat: 1.0, basic: 1.0, bug: 0.67, dragon: 0.67, air: 0.67, metal: 0.67 },
-    electric: { fire: 1.0,  water: 1.5,  grass: 0.67, electric: 0.67, combat: 1.0, basic: 1.0, bug: 1.0, dragon: 0.67, air: 1.5, metal: 1.0 },
-    combat:   { fire: 1.0,  water: 1.0,  grass: 1.0,  electric: 1.0, combat: 0.67, basic: 1.5, bug: 1.5, dragon: 0.67, air: 0.67, metal: 1.5 },
-    basic:    { fire: 1.0,  water: 1.0,  grass: 1.0,  electric: 1.0, combat: 1.0, basic: 1.0, bug: 1.0, dragon: 1.0, air: 1.0, metal: 0.67 },
-    bug:      { fire: 0.67, water: 1.0,  grass: 1.5,  electric: 1.0, combat: 1.0, basic: 1.0, bug: 1.0, dragon: 1.0, air: 0.67, metal: 0.67 },
-    dragon:   { fire: 1.0,  water: 1.0,  grass: 1.0,  electric: 1.0, combat: 1.0, basic: 1.0, bug: 1.0, dragon: 1.5, air: 1.0, metal: 0.67 },
-    air:   { fire: 1.0,  water: 1.0,  grass: 1.5,  electric: 0.67, combat: 1.5, basic: 1.0, bug: 1.5, dragon: 1.0, air: 1.0, metal: 0.67 },
-    metal:   { fire: 0.67,  water: 1.0,  grass: 1.0,  electric: 1.0, combat: 0.67, basic: 1.0, bug: 1.5, dragon: 1.0, air: 1.0, metal: 0.67 }
+    fire:     { fire: 0.67, water: 0.67, grass: 1.5,  electric: 1.0, combat: 1.0, basic: 1.0, bug: 1.5, dragon: 0.67, air: 1.0, metal: 1.5, stone: 0.67 },
+    water:    { fire: 1.5,  water: 0.67, grass: 0.67, electric: 1.0, combat: 1.0, basic: 1.0, bug: 1.0, dragon: 0.67, air: 1.0, metal: 1.0, stone: 1.5 },
+    grass:    { fire: 0.67, water: 1.5,  grass: 0.67, electric: 1.0, combat: 1.0, basic: 1.0, bug: 0.67, dragon: 0.67, air: 0.67, metal: 0.67, stone: 1.5 },
+    electric: { fire: 1.0,  water: 1.5,  grass: 0.67, electric: 0.67, combat: 1.0, basic: 1.0, bug: 1.0, dragon: 0.67, air: 1.5, metal: 1.0, stone: 1.0 },
+    combat:   { fire: 1.0,  water: 1.0,  grass: 1.0,  electric: 1.0, combat: 0.67, basic: 1.5, bug: 1.5, dragon: 0.67, air: 0.67, metal: 1.5, stone: 1.5 },
+    basic:    { fire: 1.0,  water: 1.0,  grass: 1.0,  electric: 1.0, combat: 1.0, basic: 1.0, bug: 1.0, dragon: 1.0, air: 1.0, metal: 0.67, stone: 1.0 },
+    bug:      { fire: 0.67, water: 1.0,  grass: 1.5,  electric: 1.0, combat: 1.0, basic: 1.0, bug: 1.0, dragon: 1.0, air: 0.67, metal: 0.67, stone: 0.67 },
+    dragon:   { fire: 1.0,  water: 1.0,  grass: 1.0,  electric: 1.0, combat: 1.0, basic: 1.0, bug: 1.0, dragon: 1.5, air: 1.0, metal: 0.67, stone: 1.0 },
+    air:      { fire: 1.0,  water: 1.0,  grass: 1.5,  electric: 0.67, combat: 1.5, basic: 1.0, bug: 1.5, dragon: 1.0, air: 1.0, metal: 0.67, stone: 0.67 },
+    metal:    { fire: 0.67, water: 1.0,  grass: 1.0,  electric: 1.0, combat: 0.67, basic: 1.0, bug: 1.5, dragon: 1.0, air: 1.0, metal: 0.67, stone: 0.67 },
+    stone:    { fire: 1.5,  water: 0.67, grass: 0.67, electric: 1.0, combat: 0.67, basic: 1.0, bug: 1.5, dragon: 1.0, air: 1.5, metal: 0.67, stone: 1.0 }
 };
+
 const TYPE_COLORS = {
     fire:     "#ff6b35",
     water:    "#00b4d8",
@@ -19,8 +21,9 @@ const TYPE_COLORS = {
     basic:    "#ffffff",
     bug:      "#27ae60",
     dragon:   "#9b59b6",
-    air:   "#d5ffff",
-    metal: "#c0c0c0"
+    air:      "#d5ffff",
+    metal:    "#c0c0c0",
+    stone:    "#95a5a6"
 };
 
 let isActionLocked = false; // Prevents spamming and input abuse during animations
@@ -47,7 +50,7 @@ const PETS = {
         catchRate: 45,
         evolvingLevel: 16,
         evolutionId: "pyrodon",
-        size: 1.0, // Added size stat[cite: 1]
+        size: 1.0,
         baseStats: { hp: 100, attack: 10, defense: 10, spAttack: 16, spDefense: 12, speed: 15 },
         maxStats:  { hp: 500, attack: 110, defense: 120, spAttack: 190, spDefense: 140, speed: 175 },
         moves: [
@@ -66,7 +69,7 @@ const PETS = {
         catchRate: 25,
         evolvingLevel: 20,
         evolutionId: "",
-        size: 1.5, // Added size stat[cite: 1]
+        size: 1.5,
         baseStats: { hp: 160, attack: 15, defense: 16, spAttack: 24, spDefense: 18, speed: 22 },
         maxStats:  { hp: 750, attack: 150, defense: 160, spAttack: 240, spDefense: 180, speed: 220 },
         moves: [
@@ -75,6 +78,45 @@ const PETS = {
             { name: "Flame Dash", type: "fire", damage: 45, powerCost: 10, damageType: "physical", levelToLearn: 5 },
             { name: "Inferno Blast", type: "fire", damage: 70, powerCost: 20, damageType: "special", levelToLearn: 10 },
             { name: "Burn Out", type: "fire", damage: 200, powerCost: 100, damageType: "special", levelToLearn: 1 }
+        ]
+    },
+    malime: {
+        id: "malime",
+        name: "Malime",
+        type: ["fire"],
+        spawn_routes: [1],
+        img: "petpng/malime.png",
+        catchRate: 45,
+        evolvingLevel: 16,
+        evolutionId: "magmud",
+        size: 1.0,
+        baseStats: { hp: 105, attack: 12, defense: 14, spAttack: 15, spDefense: 13, speed: 14 },
+        maxStats:  { hp: 520, attack: 120, defense: 140, spAttack: 180, spDefense: 150, speed: 160 },
+        moves: [
+            { name: "Scratch", type: "basic", damage: 10, powerCost: 0, damageType: "physical", levelToLearn: 1 },
+            { name: "Ember", type: "fire", damage: 25, powerCost: 5, damageType: "special", levelToLearn: 1 },
+            { name: "Rock Throw", type: "stone", damage: 40, powerCost: 10, damageType: "physical", levelToLearn: 5 },
+            { name: "Flame Dash", type: "fire", damage: 45, powerCost: 10, damageType: "physical", levelToLearn: 10 }
+        ]
+    },
+    magmud: {
+        id: "magmud",
+        name: "Magmud",
+        type: ["fire", "stone"],
+        spawn_routes: [],
+        img: "petpng/magmud.png",
+        catchRate: 25,
+        evolvingLevel: 0,
+        evolutionId: "",
+        size: 1.6,
+        baseStats: { hp: 165, attack: 18, defense: 22, spAttack: 22, spDefense: 20, speed: 18 },
+        maxStats:  { hp: 780, attack: 160, defense: 200, spAttack: 220, spDefense: 190, speed: 190 },
+        moves: [
+            { name: "Scratch", type: "basic", damage: 10, powerCost: 0, damageType: "physical", levelToLearn: 1 },
+            { name: "Ember", type: "fire", damage: 25, powerCost: 5, damageType: "special", levelToLearn: 1 },
+            { name: "Rock Throw", type: "stone", damage: 40, powerCost: 10, damageType: "physical", levelToLearn: 5 },
+            { name: "Flame Dash", type: "fire", damage: 45, powerCost: 10, damageType: "physical", levelToLearn: 10 },
+            { name: "Stone Edge", type: "stone", damage: 90, powerCost: 50, damageType: "physical", levelToLearn: 16 }
         ]
     },
     bubbitty: {
@@ -86,7 +128,7 @@ const PETS = {
         catchRate: 45,
         evolvingLevel: 16,
         evolutionId: "tideleel",
-        size: 0.8, // Added size stat[cite: 1]
+        size: 0.8,
         baseStats: { hp: 110, attack: 10, defense: 14, spAttack: 12, spDefense: 16, speed: 10 },
         maxStats:  { hp: 540, attack: 130, defense: 170, spAttack: 150, spDefense: 195, speed: 240 },
         moves: [
@@ -105,7 +147,7 @@ const PETS = {
         catchRate: 25,
         evolvingLevel: 0,
         evolutionId: "",
-        size: 1.3, // Added size stat[cite: 1]
+        size: 1.3,
         baseStats: { hp: 170, attack: 14, defense: 20, spAttack: 18, spDefense: 22, speed: 15 },
         maxStats:  { hp: 780, attack: 160, defense: 210, spAttack: 190, spDefense: 240, speed: 280 },
         moves: [
@@ -125,7 +167,7 @@ const PETS = {
         catchRate: 45,
         evolvingLevel: 5,
         evolutionId: "floraplnt",
-        size: 0.9, // Added size stat[cite: 1]
+        size: 0.9,
         baseStats: { hp: 120, attack: 16, defense: 13, spAttack: 8, spDefense: 10, speed: 12 },
         maxStats:  { hp: 600, attack: 185, defense: 155, spAttack: 100, spDefense: 130, speed: 145 },
         moves: [
@@ -144,7 +186,7 @@ const PETS = {
         catchRate: 25,
         evolvingLevel: 0,
         evolutionId: "",
-        size: 1.4, // Added size stat[cite: 1]
+        size: 1.4,
         baseStats: { hp: 180, attack: 22, defense: 18, spAttack: 12, spDefense: 15, speed: 16 },
         maxStats:  { hp: 820, attack: 230, defense: 190, spAttack: 130, spDefense: 170, speed: 180 },
         moves: [
@@ -163,7 +205,7 @@ const PETS = {
         catchRate: 40,
         evolvingLevel: 8,
         evolutionId: "",
-        size: 0.85, // Added size stat[cite: 1]
+        size: 0.85,
         baseStats: { hp: 95, attack: 13, defense: 8, spAttack: 17, spDefense: 9, speed: 18 },
         maxStats:  { hp: 450, attack: 150, defense: 105, spAttack: 205, spDefense: 115, speed: 210 },
         moves: [
@@ -182,7 +224,7 @@ const PETS = {
         catchRate: 35,
         evolvingLevel: 10,
         evolutionId: "",
-        size: 1.5, // Added size stat[cite: 1]
+        size: 1.5,
         baseStats: { hp: 120, attack: 17, defense: 25, spAttack: 17, spDefense: 25, speed: 10 },
         maxStats:  { hp: 650, attack: 150, defense: 180, spAttack: 150, spDefense: 180, speed: 120 },
         moves: [
@@ -202,7 +244,7 @@ const PETS = {
         catchRate: 45,
         evolvingLevel: 7,
         evolutionId: "samupod",
-        size: 0.7, // Added size stat[cite: 1]
+        size: 0.7,
         baseStats: { hp: 80, attack: 15, defense: 12, spAttack: 12, spDefense: 5, speed: 5 },
         maxStats:  { hp: 350, attack: 125, defense: 120, spAttack: 95, spDefense: 60, speed: 100 },
         moves: [
@@ -221,7 +263,7 @@ const PETS = {
         catchRate: 45,
         evolvingLevel: 12,
         evolutionId: "",
-        size: 1.6, // Added size stat[cite: 1]
+        size: 1.6,
         baseStats: { hp: 90, attack: 19, defense: 30, spAttack: 5, spDefense: 15, speed: 15 },
         maxStats:  { hp: 450, attack: 125, defense: 250, spAttack: 95, spDefense: 70, speed: 150 },
         moves: [
@@ -240,7 +282,7 @@ const PETS = {
         catchRate: 45,
         evolvingLevel: 20,
         evolutionId: "",
-        size: 1.0, // Added size stat[cite: 1]
+        size: 1.0,
         baseStats: { hp: 90, attack: 19, defense: 30, spAttack: 25, spDefense: 30, speed: 30 },
         maxStats:  { hp: 450, attack: 125, defense: 120, spAttack: 145, spDefense: 120, speed: 220 },
         moves: [
@@ -259,7 +301,7 @@ const PETS = {
         catchRate: 45,
         evolvingLevel: 20,
         evolutionId: "trishock",
-        size: 1.0, // Added size stat[cite: 1]
+        size: 1.0,
         baseStats: { hp: 100, attack: 25, defense: 30, spAttack: 25, spDefense: 15, speed: 30 },
         maxStats:  { hp: 500, attack: 130, defense: 150, spAttack: 145, spDefense: 75, speed: 250 },
         moves: [
@@ -278,7 +320,7 @@ const PETS = {
         catchRate: 45,
         evolvingLevel: 20,
         evolutionId: "",
-        size: 1.5, // Added size stat[cite: 1]
+        size: 1.5,
         baseStats: { hp: 150, attack: 35, defense: 45, spAttack: 35, spDefense: 20, speed: 45 },
         maxStats:  { hp: 800, attack: 200, defense: 190, spAttack: 210, spDefense: 90, speed: 320 },
         moves: [
@@ -298,7 +340,7 @@ const PETS = {
         catchRate: 45,
         evolvingLevel: 16,
         evolutionId: "bladee",
-        size: 0.8, // Added size stat[cite: 1]
+        size: 0.8,
         baseStats: { hp: 100, attack: 15, defense: 45, spAttack: 15, spDefense: 45, speed: 5 },
         maxStats:  { hp: 500, attack: 120, defense: 190, spAttack: 120, spDefense: 190, speed: 80 },
         moves: [
@@ -318,7 +360,7 @@ const PETS = {
         catchRate: 45,
         evolvingLevel: 20,
         evolutionId: "",
-        size: 1.5, // Added size stat[cite: 1]
+        size: 1.5,
         baseStats: { hp: 150, attack: 25, defense: 35, spAttack: 15, spDefense: 20, speed: 35 },
         maxStats:  { hp: 600, attack: 190, defense: 180, spAttack: 120, spDefense: 130, speed: 270 },
         moves: [
@@ -353,7 +395,7 @@ function getCalculatedPet(petData) {
         name: species.name,
         type: species.type,
         img: species.img,
-        size: species.size || 1.0, // Expose size stat in calculated instance[cite: 1]
+        size: species.size || 1.0,
         level: level,
         maxHp: maxHp,
         currentHp: petData.hp !== undefined ? Math.min(petData.hp, maxHp) : maxHp,
